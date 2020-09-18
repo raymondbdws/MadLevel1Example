b055bca40f4672af4c4408f2ad7d42d1a75291bc
raymondbdws <raymondbdws@hotmail.com>
2020-09-18 10:39:39 +0200

    upload

---
+++ app/src/main/java/com/rayray/madlevel1example/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.rayray.madlevel1example
+
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.rayray.madlevel1example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
+    }
+
+    private fun checkAnswer(){
+        val answer = binding.etAnswer.text.toString()
+
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+
+}
